fb55bea39d75c9742261879456d1361affd77476
Gage Rowden <140093724+gage1145@users.noreply.github.com>
2025-03-25 11:55:30 -0500

    Created the figures script.

---
+++ figures.R
@@ -0,0 +1,62 @@
+library(tidyverse)
+
+
+
+meta <- read.csv("data/meta.csv", check.names = FALSE) %>%
+  mutate_at("Sample IDs", as.character) %>%
+  na.omit()
+
+df_ <- read.csv("data/data.csv", check.names = FALSE) %>%
+  mutate_at("Sample IDs", ~str_remove(., " ")) %>%
+  filter(!(`Sample IDs` %in% c("N", "P"))) %>%
+  left_join(meta, "Sample IDs")
+  # separate_wider_delim(
+  #   `Period in environment`,
+  #   " - ",
+  #   names = c("start", "end")
+  # )
+
+
+
+
+df_ %>%
+  separate_wider_delim(
+    ID, 
+    " ", 
+    names = c("Feeder", "Plate"), 
+    too_few = "align_start", 
+    too_many = "merge"
+  ) %>%
+  # filter(Date != unique(Date)[1]) %>%
+  ggplot(aes(Feeder, RAF, fill = Plate)) +
+  geom_boxplot() +
+  facet_grid(
+    cols = vars(`Period in environment`), 
+    rows = vars(Date), 
+    space = "free_x", 
+    scales = "free"
+  )
+
+# df_ %>%
+#   filter(Date != unique(Date)[1]) %>%
+#   ggplot(aes(`Sample IDs`, RAF, fill = Date)) +
+#   geom_boxplot() +
+#   facet_grid(~`ID`, space = "free_x", scales = "free")
+
+
+
+# Plate Views -------------------------------------------------------------
+
+
+
+for (file in list.files("raw", full.names = TRUE)) {
+  
+  filename <- str_split_i(file, "/", 2) %>%
+    str_remove(".xlsx")
+  locations <- get_sample_locations(file)
+  raw <- get_real(file)[[1]]
+  
+  plate_view(raw, locations) +
+    ggtitle(file)
+  ggsave(paste0("figures/", filename, "_plate_view.png"), width = 12, height = 8)
+}
